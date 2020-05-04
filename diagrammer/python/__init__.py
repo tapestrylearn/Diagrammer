19a719d09f39d1833d1d6a1f9ff70251a8c5118d
Arthur Lafrance <lafrancearthur@gmail.com>
2020-05-03 19:43:17 -0700

    fixed naming bug

--- diagrammer/python/__init__.py
+++ diagrammer/python/__init__.py
@@ -1,12 +1,12 @@
 from . import model, engine, scene
 
 def generate_diagrams_for_code(code: str, flags: [int]) -> dict:
-    engine = PythonEngine()
-    engine.run(code, flags)
+    py_engine = engine.PythonEngine()
+    py_engine.run(code, flags)
 
     diagram_data = []
 
-    for snapshot_data in engine.get_bare_language_data():
+    for snapshot_data in py_engine.get_bare_language_data():
         globals_data, locals_data = (snapshot_data['globals'], snapshot_data['locals'])
         snapshot = scene.PySnapshot(globals_data, locals_data)
 
