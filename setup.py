e4f1e17394e53a833492f66fc07296be1efe047a
Arthur Lafrance <lafrancearthur@gmail.com>
2020-06-04 15:37:52 -0700

    added setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+
+setuptools.setup(
+    name='diagrammer',
+    version='0.0.1',
+
+    author='Tapestry',
+    author_email='tech@tapestrylearn.com',
+
+    description="Tapestry's proprietary diagram generation system",
+    long_description="Tapestry's proprietary diagram generation system",
+    url='https://github.com/tapestrylearn/Diagrammer',
+
+    packages=setuptools.find_packages(),
+
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+
+    python_requires='>=3.6',
+)
