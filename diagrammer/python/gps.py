591641513c76ca9ea5d336a190f099d664354abb
wangpatrick57 <wang.patrick57@gmail.com>
2020-07-03 16:38:38 -0700

    finished basic nsga loop

--- diagrammer/python/gps.py
+++ diagrammer/python/gps.py
@@ -44,6 +44,18 @@ class Solution:
     def add_to_Sp(self, solution: 'Solution') -> None:
         self._Sp.add(solution)
 
+    def set_front_num(self, front_num: int) -> None:
+        self._front_num = front_num
+
+    def get_front_num(self) -> int:
+        return self._front_num
+
+    def get_position_array(self) -> [(float, float)]:
+        return self._position_array
+
+    def get_position(self, index: int) -> (float, float):
+        return self._position_array[index]
+
     def np(self) -> int:
         return self._np
 
@@ -63,9 +75,11 @@ class Solution:
 # MAIN ALGORITHM
 # note: GPS can access internal vars of PyScene since it's pretty much an internal class, it's just in its own module because there's so much code
 class GPS:
-    GENERATION_NUM = 1
-    POP_SIZE = 5
+    GENERATION_NUM = 5
+    POP_SIZE = 20
     FITNESS_FUNCTIONS = [get_num_intersections]
+    MUTATION_CHANCE = 0.05
+    MUTATION_MAGNITUDE = 5
 
     def __init__(self, scne: scene.PyScene, scene_name: str):
         print(scene_name)
@@ -87,8 +101,9 @@ class GPS:
         local_visualizer.remove_base_dir()
 
         for g in range(GPS.GENERATION_NUM):
-            # sort
             self._non_dominated_sort()
+            self._select()
+            self._repopulate()
 
             # generate images
             for front_num, front in self._fronts.items():
@@ -106,20 +121,17 @@ class GPS:
 
         # sort
         current_front = 0
+        s = 0
 
-        for sol in self._solutions:
+        while s < len(self._solutions):
+            sol = self._solutions[s]
+
             if sol.np() == 0:
-                self._fronts[current_front].add(sol)
-
-        for sol in self._fronts[current_front]:
-            self._solutions.remove(sol)
+                self._add_to_front(current_front, sol)
+            else:
+                s += 1
 
         while len(self._solutions) != 0:
-            if current_front < 10:
-                print(f'\nwhile start {current_front}')
-                print('solutions', json.dumps([str(sol) for sol in self._solutions], indent = 2))
-                print('fronts', json.dumps([f'{front_num} : {[str(sol) for sol in solutions]}' for front_num, solutions in self._fronts.items()], indent = 2))
-
             for sol in self._fronts[current_front]:
                 for dominated_sol in sol._Sp:
                     dominated_sol.inc_np(-1)
@@ -129,6 +141,52 @@ class GPS:
 
             current_front += 1
 
+    def _select(self) -> None:
+        for front in self._fronts.values():
+            for sol in front:
+                self._solutions.append(sol)
+
+                if len(self._solutions) >= GPS.POP_SIZE / 2:
+                    return
+
+    def _repopulate(self) -> None:
+        child_solutions = []
+
+        while len(child_solutions) < GPS.POP_SIZE - len(self._solutions):
+            # pick parents
+            parents = [None] * 2
+
+            for p in range(2):
+                candidate1 = self._solutions[random.randrange(len(self._solutions))]
+                candidate2 = self._solutions[random.randrange(len(self._solutions))]
+
+                if candidate1.get_front_num() < candidate2.get_front_num():
+                    parents[p] = candidate1
+                else:
+                    parents[p] = candidate2
+
+            # create child_position_array with shuffled genes
+            gene_indices = [i for i in range(len(parents[0].get_position_array()))]
+            random.shuffle(gene_indices)
+            child_position_array = [None] * len(gene_indices)
+
+            for gene_index in gene_indices:
+                if gene_index < len(gene_indices) / 2:
+                    child_position_array[gene_index] = parents[0].get_position(gene_index)
+                else:
+                    child_position_array[gene_index] = parents[1].get_position(gene_index)
+
+            # mutate child
+            for gene_index in range(len(child_position_array)):
+                if random.random() < GPS.MUTATION_CHANCE:
+                    child_position_array[gene_index] = (child_position_array[gene_index][0] + random.gauss(0, 1) * GPS.MUTATION_MAGNITUDE, child_position_array[gene_index][1] + random.gauss(0, 1) * GPS.MUTATION_MAGNITUDE)
+
+            # add child object to child_solutions
+            child_solutions.append(Solution(self._scne, child_position_array, GPS.FITNESS_FUNCTIONS))
+
+        self._solutions.extend(child_solutions)
+
     def _add_to_front(self, front_num: int, sol: Solution) -> None:
         self._fronts[front_num].add(sol)
         self._solutions.remove(sol)
+        sol.set_front_num(front_num)
